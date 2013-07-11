72a20f3655ec760312bc594c83c10b8f412ee090
wokumura <theprofessional9@gmail.com>
2013-07-10 18:15:31 -1000

    Completed hw3

--- hw3/grader.js
+++ hw3/grader.js
@@ -28,20 +28,6 @@ var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
-var checkUrl = function(url, checksfile) {
-    outp = rest.get(url).on('complete', function(result) {
-        $ = cheerio.load(result);
-        var checks = loadChecks(checksfile).sort();
-        var out = {};
-        for (var ii in checks) {
-            var present = $(checks[ii]).length > 0;
-            out[checks[ii]] = present;
-        }
-        //return out;
-	var outJson = JSON.stringify(out, null, 4);
-	console.log(outJson);
-    });
-};
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -77,15 +63,31 @@ var clone = function(fn) {
     return fn.bind({});
 };
 
+
+var checkUrl = function(url, checksfile) {
+    rest.get(url).on('complete', function(result) {
+        $ = cheerio.load(result);
+        var checks = loadChecks(checksfile).sort();
+        var out = {};
+        for (var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+        }
+	var outJson = JSON.stringify(out, null, 4);
+	console.log(outJson);
+    });
+};
+
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        // for testing use http://rocky-plateau-8998.herokuapp.com/
         .option('-u, --url <url_addr>', 'URL to check')
         .parse(process.argv);
     if (program.url) {
 	checkUrl(program.url, program.checks);
-	//console.log("Got here");
     } else {
         var checkJson = checkHtmlFile(program.file, program.checks);
         var outJson = JSON.stringify(checkJson, null, 4);
